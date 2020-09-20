bf0051a7fd0e1279f95aaa085c675b97df2c6da2
wikky2012 <wicliferono@gmail.com>
2020-09-20 18:56:36 +0300

    initial commits

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -16,9 +22,10 @@ def create_app(config_name):
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
 
-    # Initializing flask extensions
+    #Initializing Flask Extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
 from . import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -45,12 +52,14 @@ class Review:
 
         return response
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
 
     pass_secure  = db.Column(db.String(255))
 
